a3d096e726a0cb6b11a0b73a40f6f4e1160b59e4
Vadim <vpodgurskiy@gmail.com>
2022-05-14 18:02:11 +0300

    fixed json properties naming

--- src/main/java/com/nosto/currency/converter/CurrencyConvertor/models/CurrencyDto.java
+++ src/main/java/com/nosto/currency/converter/CurrencyConvertor/models/CurrencyDto.java
@@ -12,8 +12,6 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @JsonDeserialize(using = CurrencyConvertorDeserializer.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
@@ -23,11 +21,70 @@ public class CurrencyDto {
   LocalDate date;
   Double rate;
   Integer amount;
-  @JsonProperty("from")
   String currencyFrom;
-  @JsonProperty("to")
   String currencyTo;
-  @JsonProperty("result")
   Double monetaryValue;
   ExchangeRatesError error;
+
+  @JsonProperty("monetaryValue")
+  public Double getMonetaryValue() {
+    return monetaryValue;
+  }
+
+  @JsonProperty("result")
+  public Double setMonetaryValue(Double monetaryValue) {
+    return this.monetaryValue = monetaryValue;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public Double getRate() {
+    return rate;
+  }
+
+  public void setRate(Double rate) {
+    this.rate = rate;
+  }
+
+  public Integer getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Integer amount) {
+    this.amount = amount;
+  }
+
+  @JsonProperty("currencyFrom")
+  public String getCurrencyFrom() {
+    return currencyFrom;
+  }
+
+  @JsonProperty("from")
+  public void setCurrencyFrom(String currencyFrom) {
+    this.currencyFrom = currencyFrom;
+  }
+
+  @JsonProperty("currencyTo")
+  public String getCurrencyTo() {
+    return currencyTo;
+  }
+
+  @JsonProperty("to")
+  public void setCurrencyTo(String currencyTo) {
+    this.currencyTo = currencyTo;
+  }
+
+  public ExchangeRatesError getError() {
+    return error;
+  }
+
+  public void setError(ExchangeRatesError error) {
+    this.error = error;
+  }
 }
